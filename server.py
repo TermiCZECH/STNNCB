3efad5d3881a795400b6614f63b774d72d694d6d
TermiCZECH <121626683+TermiCZECH@users.noreply.github.com>
2023-05-16 23:04:02 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import subprocess
+import socket
 
 app = Flask(__name__)
 CORS(app)
@@ -28,6 +29,7 @@ params = {
 # Default configuration parameters
 default_config = {}
 
+
 def load_default_config():
     global default_config
     try:
@@ -37,6 +39,7 @@ def load_default_config():
     except FileNotFoundError:
         print("Default configuration file 'default_config.py' not found.")
 
+
 def generate_python_config_string(config_dict):
     config_string = "params = {\n"
     for key, value in config_dict.items():
@@ -44,20 +47,39 @@ def generate_python_config_string(config_dict):
     config_string += "}"
     return config_string
 
+
 def update_config_file():
     config_string = generate_python_config_string(params)
     with open("config.py", "w") as config_file:
         config_file.write(config_string)
+
 
 def start_chatbot_process():
     # Change the path and command accordingly
     with open("chatbot_output.txt", "w") as output_file:
         subprocess.Popen(["python", "STNNCB early. 0.1.py"], stdout=output_file)
 
+
+# DNS resolution function using Google DNS (8.8.8.8)
+def resolve_dns(hostname):
+    try:
+        resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        resolver.connect(("8.8.8.8", 53))
+        resolver.sendall(b"\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00"
+                         + hostname.encode("utf-8") + b"\x00\x00\x01\x00\x01")
+        data = resolver.recv(1024)
+        resolver.close()
+        ip_address = socket.inet_ntoa(data[21:25])
+        return ip_address
+    except socket.error:
+        return None
+
+
 # Endpoint to retrieve the current configuration
 @app.route('/get_config', methods=['GET'])
 def get_config():
     return jsonify(params)
+
 
 # Endpoint to update the configuration
 @app.route('/update_config', methods=['POST'])
@@ -67,6 +89,7 @@ def update_config_endpoint():
     update_config_file()
     return jsonify(success=True)
 
+
 # Endpoint to send a message and receive a response
 @app.route('/send_message', methods=['POST'])
 def send_message():
@@ -75,6 +98,7 @@ def send_message():
     response = process_message(message)  # Replace with your message processing logic
     return jsonify({'response': response})
 
+
 # Endpoint to retrieve messages from chatbot_output.txt
 @app.route('/get_chatbot_output', methods=['GET'])
 def get_chatbot_output():
@@ -82,11 +106,28 @@ def get_chatbot_output():
         chatbot_output = output_file.read()
     return jsonify({'chatbot_output': chatbot_output})
 
+
+# DNS resolution function using Google DNS (8.8.8.8)
+def resolve_dns(hostname):
+    try:
+        resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        resolver.connect(("8.8.8.8", 53))
+        resolver.sendall(b"\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00"
+                         + hostname.encode("utf-8") + b"\x00\x00\x01\x00\x01")
+        data = resolver.recv(1024)
+        resolver.close()
+        ip_address = socket.inet_ntoa(data[21:25])
+        return ip_address
+    except socket.error:
+        return None
+
+
 # Endpoint to start the chatbot
 @app.route('/start_chatbot', methods=['GET'])
 def start_chatbot():
     start_chatbot_process()
     return jsonify(success=True)
+
 
 # Endpoint to configure the settings
 @app.route('/configure_settings', methods=['POST'])
@@ -96,5 +137,6 @@ def configure_settings():
     update_config_file()
     return jsonify(success=True)
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=11000)
+    app.run(host=resolve_dns("your-domain.com"), port=11000)
